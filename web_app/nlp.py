f462e3bf051389e9eb0b9dd3f5178255e51d86de
Md kaif khan <35622849+kaif3007@users.noreply.github.com>
2020-02-27 21:25:23 +0530

    Create nlp.py

---
+++ web_app/nlp.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import spacy,pickle
+import numpy as np
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+
+
+
+def predict_sentiment(ques):
+	word_dict=pickle.load(open('word_dict.txt','rb'))
+	text_list=[]
+	text_list.append(ques)
+	nlp = spacy.load('en', disable=['parser','ner','tagger'])
+	nlp.vocab.add_flag(lambda s:s.lower() in spacy.lang.en.stop_words.STOP_WORDS,spacy.attrs.IS_STOP)
+
+	docs=nlp.pipe(text_list,n_threads=2)
+
+	word_sequences=[]
+
+	for doc in docs:
+		word_seq=[]
+		for token in doc:
+			if (token.text in word_dict) and (token.pos_ is not "PUNCT"):
+				word_seq.append(word_dict[token.text])
+			if (token.text not in word_dict) and (token.pos_ is not "PUNCT"):
+				word_seq.append(0)
+		word_sequences.append(word_seq)
+
+	word_sequences=pad_sequences(word_sequences, maxlen=55, padding='post')
+	model=load_model('model0.h5')
+	pred_prob=np.squeeze(model.predict(word_sequences,verbose=1))
+	if pred_prob>.35:
+		return "Insincere question.",pred_prob
+	else:
+		return "Genuine question.",pred_prob
+	
